b5bc9632a5b047a84932e843ea62ef2b47c5cb7f
hello30190f <108202763+hello30190f@users.noreply.github.com>
2026-01-01 18:48:22 +0900

    implement the network setting interface partially

--- src/module/mainInfo.tsx
+++ src/module/mainInfo.tsx
@@ -1,5 +1,4 @@
 
-
 export function MainInfo(){
 
     return <div className="MainInfo main w-screen fixed top-0 left-0 h-screen pt-[5rem] bg-gray-800">
--- src/module/sidebar.tsx
+++ src/module/sidebar.tsx
@@ -3,9 +3,45 @@ import { useMainData } from "../App"
 
 export function Sidebar(){
     const sidebar = useMainData((s) => s.sidebar)
+    const setMainData = useMainData.setState
+    const network = useMainData((s) => s.network)
 
     if(sidebar)
-        return <div className="sidebar fixed top-0 left-0 h-screen min-w-[10rem] bg-gray-900">
-
+        return <div className="sidebar fixed top-0 left-0 h-screen min-w-[15rem] bg-gray-900">
+            <div className="settings text-3xl font-bold pt-6 pl-1 text-start">Network Settings:</div>
+            <div className="detail m-2">
+                <div className="Interval text-center m-2 p-2 rounded-2xl bg-gray-800">
+                    <div className="explain text-xl font-bold">
+                        Update Interval
+                    </div>
+                    <div className="flex w-full">
+                        <input 
+                            className="text-xl text-center font-bold" 
+                            placeholder="Please enter update interval" 
+                            id="interval" type="number"
+                            value={network.updateInterval}
+                            onChange={(event:React.ChangeEvent<HTMLInputElement>) => {
+                                setMainData({
+                                    network:{
+                                        ...network,
+                                        updateInterval: Number(event.target.value)
+                                    }
+                                })
+                            }}
+                            ></input>
+                        <div className="text-xl font-bold">sec</div>
+                    </div>
+                </div>
+                <div className="Address text-center m-2 p-2 rounded-2xl bg-gray-800">
+                    <div className="explain text-xl font-bold">
+                        IP Address
+                    </div>
+                </div>
+                <div className="Port text-center m-2 p-2 rounded-2xl bg-gray-800">
+                    <div className="explain text-xl font-bold">
+                        Port
+                    </div>
+                </div>
+            </div>
         </div>
 }
