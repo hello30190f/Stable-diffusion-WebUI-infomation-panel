13cfe48faecae0fd85ac05eb812dba5c135e8e97
hello30190f <108202763+hello30190f@users.noreply.github.com>
2026-01-05 05:28:21 +0900

    update image preview to show image in fullsize

--- src/module/imagePreview.tsx
+++ src/module/imagePreview.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useMainData } from "../App"
 import brokenImage from "../assets/brokenimage.png"
 
@@ -12,6 +12,12 @@ export function ImagePreview(){
     
     const [imageURL,setImageURL] = useState(brokenImage)
     const [style,setStyle] = useState("ImagePreveiw flex justify-center" + horizontalStyle)
+
+    const imageRef = useRef<HTMLImageElement | null>(null)
+    const [imageElemStyle,setImageElemStyle] = useState({
+        width: "",
+        height: ""
+    })
 
     useEffect(() => {
         if(screenOrientation == "Horizontal"){
@@ -39,20 +45,44 @@ export function ImagePreview(){
     useEffect(() => {
         if(APIresponse == null){
             setImageURL(brokenImage)
+            setImageElemStyle({
+                width: "",
+                height: ""
+            })
             return
         }
 
         if(APIresponse.current_image == "" ||  APIresponse.current_image == "null"){
             setImageURL(brokenImage)
+            setImageElemStyle({
+                width: "",
+                height: ""
+            })
             return
         }
 
+        if(imageRef.current != null){
+            const ratio = imageRef.current.width / imageRef.current.height
+            if(ratio > 1){
+                setImageElemStyle({
+                    width: "100%",
+                    height: ""
+                })
+            }else{
+                setImageElemStyle({
+                    width: "",
+                    height: "100%"
+                })
+            }
+        }
         setImageURL("data:image/png;base64," + APIresponse.current_image)
     },[APIresponse])
 
+
+
     return <div className={style}>
         <div className="w-full h-full flex justify-center" style={{alignItems:"center"}}>
-            <img className="max-w-full max-h-full" src={imageURL}></img>
+            <img ref={imageRef} style={imageElemStyle} className="max-w-full max-h-full" src={imageURL}></img>
         </div>
     </div>
 }
